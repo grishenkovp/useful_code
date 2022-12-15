825bbb28e8d7797d483f3106639b3dcb66fa8fb0
Pavel Grishenkov <54138959+grishenkovp@users.noreply.github.com>
2022-12-15 13:21:07 +0500

    Create pandas_display_options.py

---
+++ pandas_display_options.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.max_colwidth', 80)
+pd.set_option('display.max_rows', 50)
+pd.get_option('display.precision', 2)
+pd.set_option('display.float_format', '{:.2f}'.format)
